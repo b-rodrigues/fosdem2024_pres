9057b67876dc768595b36d3d6024fc44ad41a7ac
Bruno Rodrigues <bruno@brodrigues.co>
2024-02-02 10:28:38 +0100

    data as taret

--- _targets.R
+++ _targets.R
@@ -2,6 +2,10 @@ library(targets)
 library(tarchetypes)
 
 list(
+  tar_target(
+    life_cycle_savings,
+    read.csv("life_cycle_savings")
+  ),
   tar_quarto(
     fosdem_pres,
     "fosdem_pres.qmd"
