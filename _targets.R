334a7a53e4bc29c9f852f2375b7d320f4f0b872d
Bruno Rodrigues <bruno@brodrigues.co>
2024-02-02 10:06:59 +0100

    render on GA

---
+++ _targets.R
@@ -0,0 +1,10 @@
+library(targets)
+library(tarchetypes)
+
+list(
+  tar_quarto(
+    fosdem_pres,
+    "fosdem_pres.qmd"
+  )
+
+)
---
+++ default.nix
@@ -0,0 +1,32 @@
+let
+ pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/06b93631a20bc9c1e73d7b5c706af12ee01922aa.tar.gz") {};
+  rix = [(pkgs.rPackages.buildRPackage {
+            name = "rix";
+            src = pkgs.fetchgit {
+              url = "https://github.com/b-rodrigues/rix";
+              branchName = "master";
+              rev = "65b3580fa07997fe0b532a5eefadf0366065be55";
+              sha256 = "sha256-2RxXaIg68S5MTnhTuozK+DSYwYL0RUTf/8xr0FQbtBA=";
+            };
+            propagatedBuildInputs = builtins.attrValues {
+              inherit (pkgs.rPackages) codetools httr jsonlite sys;
+            };
+         })
+  ];
+ system_packages = builtins.attrValues {
+  inherit (pkgs) R glibcLocalesUtf8 quarto nix;
+};
+  in
+  pkgs.mkShell {
+    LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+
+    buildInputs = [ system_packages rix pkgs.rPackages.targets pkgs.rPackages.tarchetypes];
+
+    shellHook = '' Rscript -e "targets::tar_make()" '';
+  }
